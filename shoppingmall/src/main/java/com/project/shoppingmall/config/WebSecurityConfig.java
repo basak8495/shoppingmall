4acb1beed9c004df1019be98457475fa25603461
basak8495 <basak8495@naver.com>
2023-09-20 15:14:36 +0900

    Feat: test

--- shoppingmall/src/main/java/com/project/shoppingmall/config/WebSecurityConfig.java
+++ shoppingmall/src/main/java/com/project/shoppingmall/config/WebSecurityConfig.java
@@ -25,11 +25,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                     .antMatchers("/css/**", "/img/**", "/js/**", "/lib/**", "/mail/**", "/scss/**", "/", "/account/register", "/product/list", "/support/customer_center").permitAll()
                     .anyRequest().authenticated()
-                    .and()
+                .and()
                 .formLogin()
-                .loginPage("/account/login")
+                    .loginPage("/account/login")
                     .permitAll()
-                    .and()
+                .and()
                 .logout()
                     .permitAll();
         /*http.csrf().ignoringAntMatchers("/cart/**");*/
@@ -45,7 +45,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                         + "from user "
                         + "where username = ?")
                 .authoritiesByUsernameQuery("select u.username, r.name "
-                        + "from user_role ur inner join user u on ur.user_id = u.id "
+                        + "from user_role ur "
+                        + "inner join user u on ur.user_id = u.id "
                         + "inner join role r on ur.role_id = r.id "
                         + "where u.username = ?");
     }
